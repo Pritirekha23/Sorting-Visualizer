31988bca85b206d9acc5cb4f97b5195f37c9c852
Pritirekha23 <learnpriti23@gmail.com>
2023-11-29 23:36:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var barEl = [];
+let delay = 300;
+generateBars();
+
+function generateBars(num = 30) {
+    clearBox()
+    for (let i = 0; i < num; i += 1) {
+        const value = Math.floor(Math.random() * 300) + 1;
+        const bar = document.createElement("div");
+        bar.classList.add("bar");
+        barEl.push(value);
+        bar.style.height = `${value * 3}px`;
+        bar.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.5) 100%)";
+        // bar.style.background = "linear-gradient(315deg, #00bfb2 0%, #028090 74%)";
+        bar.style.border = "1px solid black";
+        bar.style.transform = `translateX(${i * 30}px)`;
+        bar.classList.add(`barNo${i}`);
+        document.getElementById("main").appendChild(bar);
+
+    }
+}
+
+function clearBox() {
+    let bar = document.querySelector("#main");
+    bar.innerHTML = '';
+}
+
+
+
+//speeed
+let delayElement = document.querySelector('#sortSpeed');
+
+delayElement.addEventListener('input', () => {
+    delay = 360 - parseInt(delayElement.value);               // 360 - 300 min delay
+});
+
+
+
+
+
